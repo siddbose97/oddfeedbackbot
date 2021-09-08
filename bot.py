c38aeb5f799f88b11bca869f1480df24ed358734
Siddhartha Bose <siddbose97@gmail.com>
2021-09-08 16:20:35 +0800

    added webhooks

--- bot.py
+++ bot.py
@@ -306,7 +306,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                         port=PORT,
                         url_path=TOKEN,
-                        webhook_url="https://polar-chamber-36116.herokuapp.com/" + TOKEN)
+                        webhook_url="https://still-sierra-92948.herokuapp.com/" + TOKEN)
     updater.idle()
 
 
