d279acc45126566a31fce778ebefbec4d7c02708
Siddhartha Bose <siddbose97@gmail.com>
2021-10-15 11:54:53 +0800

    ammended odd values

--- armskote.py
+++ armskote.py
@@ -6,86 +6,86 @@ import buttons
 mainDB = {
     'Armour':{
         '40SAR':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW ", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW ", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         },
         '41SAR':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         },
         '42SAR':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         },
         '48SAR':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         },
     },
     'Artillery':{
         '21SA':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         }
     },
     'Engineers':{
         '30SCE':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         }
     },
     'Commandos':{
         '1st':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         }
     },
     'Guards':{
         '1st':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         }
     },
     'Infantry':{
         '1SIR':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         }
     },
     'Signals':{
         '1SIG':{
-            'HQ': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Support': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Alpha': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Bravo': ["SAR21", "SAW MK3A", "GPMG INF"],
-            'Charlie': ["SAR21", "SAW MK3A", "GPMG INF"]
+            'HQ': ["SAR21", "SAW", "GPMG INF"],
+            'Support': ["SAR21", "SAW", "GPMG INF"],
+            'Alpha': ["SAR21", "SAW", "GPMG INF"],
+            'Bravo': ["SAR21", "SAW", "GPMG INF"],
+            'Charlie': ["SAR21", "SAW", "GPMG INF"]
         }
     },
 
--- weapons.py
+++ weapons.py
@@ -1,76 +1,85 @@
 weaponDefects = {
     "SAR21": {
+        "Barrel":{
+            1:"Bent or curved",
+            2:"Handguard Loose, Cracked or Missing",
+            3:"Other"
+        },
+        "Scope":
+        {
+            1:"Lens Cracked or Scratched",
+            2:"Water in Lens",
+            3:"Scope Loose",
+            4:"Crosshairs tilted",
+            5:"Other"
+        },
+        "Charging and Carrying Handle":
+        {
+            1:"Tear in Carrying Handle (Longer than 3cm)",
+            2:"Step Pin or circlip missing",
+            3:"Front sight post broken or missing",
+            4:"Charging handle broken or missing",
+            5:"Charging handle not working",
+            6:"Other"
+        },
+        "LAD":
+        {
+            1:"Front of LAD cracked or broken",
+            2:"Selector switch not clicking into position",
+            3:"Lanyard broken or battery cap missing",
+            4:"Rubber switch not working correctly",
+            5:"LAD loose",
+            6:"LAD not working correctly",
+            7:"LAD does not work with Battery",
+            8:"Other"
+        },
+        "Muzzle":{
+            1:"Muzzle cracked or worn out",
+            2:"Washer under muzzle missing",
+            3:"Other"
+        },
+        "Bolt Carrier":{
+            1:"Cam pin missing",
+            2:"Extractor not working",
+            3:"Extractor pin broken or missing",
+            4:"Other"
+        },
+        "Upper/Lower Receiver":
+        {
+            1:"Trigger not working",
+            2:"Safety button not working",
+            3:"Safety button too tight/loose",
+            4:"Round deflector loose or missing",
+            5:"Front takedown pin too tight or missing",
+            6:"Back takedown pin too tight or missing",
+            7:"Auto-selector too tight or fails to function",
+            8:"Other"
+        },
+        "Functional Check":
+        {
+            1:"Gas regulator cannot turn",
+            2:"Can fire when set to safe",
+            3:"Last round catch not catching bolt",
+            4:"Magazine not being held in place",
+            5:"Bolt carrier cannot slide freely",
+            6:"Other"
+        },
+        "OTHER": {
+            1:"Input ODD Location"
+        }
+    },
+    "SAW": {
         "BARREL":{
             1:"Bent or curved",
             2:"Handguard Loose, Cracked or Deficient",
             3:"Others"
-        },
-        "SCOPE":
-        {
-            1:"Lens Cracked or Scratched",
-            2:"Condensation in Lens",
-            3:"Scope Loose",
-            4:"Reticle/Crosshairs tilted",
-            5:"Cannot aim target at 300m",
-            6:"Others"
-        },
-        "CHARGING HANDLE":
-        {
-            1:"Rubber handle tear > 30mm",
-            2:"Step Pin/Circlip deficient",
-            3:"Front sight post broken/deficient",
-            4:"Charging handle broken or deficient",
-            5:"Charging handle over rotated",
-            6:"Charging handle fail to function",
-            7:"Others"
-        },
-        "LAD":
-        {
-            1:"Baffle worn out or cracked",
-            2:"Selector switch worn out",
-            3:"Lanyard broken",
-            4:"Battery cap assembly deficient",
-            5:"Momentary switch torn or faulty",
-            6:"LAD loose",
-            7:"LAD (4 modes) fail function check",
-            8:"LAD does not work with AA Battery",
-            9:"Others"
-        },
-        "MUZZLE AND WASHER":{
-            1:"Muzzle cracked or worn out",
-            2:"Washer deficient",
+        }
+    },
+    "GPMG INF": {
+        "BARREL":{
+            1:"Bent or curved",
+            2:"Handguard Loose, Cracked or Deficient",
             3:"Others"
-        },
-        "BOLT CARRIER ASSEMBLY":{
-            1:"Bolt cam pin deficient",
-            2:"Extractor spring broken or deficient",
-            3:"Extractor pin broken or deficient",
-            4:"Others"
-        },
-        "RECEIVERS":
-        {
-            1:"Trigger bar assembly fail to function",
-            2:"Safety button fail to function",
-            3:"Safety button too tight/loose",
-            4:"Deflector loose/deficient",
-            5:"Front takedown pin too tight/deficient",
-            6:"Back takedown pin too tight/deficient",
-            7:"Hammer Spring Broken/deficient",
-            8:"Hammer Plunger broken/deficient",
-            9:"Auto-selector too tight or fail to function",
-            10:"Others"
-        },
-        "FC":
-        {
-            1:"Gas regulator fail to function",
-            2:"Can fire when set to safe",
-            3:"Last round catch fail to trap bolt carrier assembly",
-            4:"Magazine catch fail to hold magazine",
-            5:"Bolt carrier cannot slide freely in Charging Tube Assembly",
-            6:"Others"
-        },
-        "OTHER": {
-            1:"Input ODD Location"
         }
     }
 }
